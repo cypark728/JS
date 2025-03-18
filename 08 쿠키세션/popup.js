17424b05a24ea89f0125144d9ec81568cb51d374
cypark728 <cypark448@gmail.com>
2025-03-18 13:59:52 +0900

    2025-03-18

---
+++ 08 쿠키세션/popup.js
@@ -0,0 +1,28 @@
+var check = document.getElementById("check");
+
+function createCookie(cookieName, cookieValue, time) {
+    console.log("check");
+    var date = new Date();
+    date.setDate(date.getDate() + time);
+    date.setHours(9);
+    date.setMinutes(0);
+    date.setSeconds(0);
+
+    var cookie = "";
+    cookie += cookieName + "=" + cookieValue + "; ";
+    cookie += "expires" + date.toUTCString() + "; ";
+
+    document.cookie = cookie;
+}
+
+function getCookie(find) {
+    var cookies = document.cookie.split("; ");
+
+    for(var i in cookies) {
+        if(cookies[i].indexOf(find) != -1) { //쿠키이름이 존재함
+            var value = cookies[i].replace(find+"=", "");
+        }
+        return value; //쿠키값을 반환
+    }
+
+}
